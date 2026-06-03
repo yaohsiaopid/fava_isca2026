edf9f9fb9c075128ac1b522956dc6242311a97bb
samanthaarcher0 <146493311+samanthaarcher0@users.noreply.github.com>
2026-06-02 21:01:30 -0700

    Update python reqs in setup_venv.sh

--- setup_venv.sh
+++ setup_venv.sh
@@ -5,7 +5,8 @@ python3 -m venv pyenv/
 source pyenv/bin/activate
 python3 -m pip install --upgrade pip 
 python3 -m pip install -r qcmbr_isca26/requirements.txt
-# python3 -m pip install matplotlib pandas networkx cvc5 numpy
+python3 -m pip install matplotlib pandas networkx cvc5 numpy
+python3 -c "import networkx; import numpy; import matplotlib; import cvc5; import pandas" 
 else
   source pyenv/bin/activate
 fi
